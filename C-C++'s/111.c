f7859755c8e2c90a3acfb67e9b7bb9f9e719f040
Hamoodteen <ahmed104567@outlook.com>
2024-03-04 22:47:01 +0200

    0

--- C-C++'s/111.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-
-int f(int X , int Y)
-{
-    return (pow(X , Y) + sqrt(X));
-}
--- Web's/README.md
+++
@@ -1,3 +0,0 @@
-# أتعلم الويب من الزيرو , ALX
-
-<html><head><title>My Web Projects</title></head></html>
